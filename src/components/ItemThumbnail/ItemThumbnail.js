892e9f484b5bd873cc92297eddbde6f113364d69
rhyso <rhyswork@gmail.com>
2020-06-01 19:57:26 +0100

    removed price from homepage thumbnails

--- src/components/ItemThumbnail/ItemThumbnail.js
+++ src/components/ItemThumbnail/ItemThumbnail.js
@@ -63,7 +63,6 @@ const itemThumbnail = (props) => {
                 <ImgStyled fluid={props.image} />
                 <Heading>{props.heading}</Heading>
             </LinkStyled>
-            <Price>£{props.price.toFixed(2)}</Price>
         </ItemThumbnailStyled >
     )
 }
